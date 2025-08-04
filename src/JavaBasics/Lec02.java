a682665d64b55da9091d503a5396007cdfa02a9a
Achyut Sudele <98205409+Sparkzud@users.noreply.github.com>
2025-08-04 23:56:22 +0530

    second commit

---
+++ src/JavaBasics/Lec02.java
@@ -0,0 +1,19 @@
+package JavaBasics;
+
+public class Lec02 {
+    public static void main(String[] args){
+
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+
+        System.out.println("--------------");
+
+        //use only one print statement
+
+        System.out.print("*\n**\n***\n****\n*****");
+
+    }
+}
