0c3c065a04f662f3e567f0c396b768485d6b1990
Achyut Sudele <98205409+Sparkzud@users.noreply.github.com>
2025-08-05 00:02:06 +0530

    commit

---
+++ src/JavaBasics/Lec03.java
@@ -0,0 +1,14 @@
+package JavaBasics;
+
+public class Lec03 {
+    public static void main(String[] args)
+    {
+        //print z with stars
+        
+        System.out.println("*****");
+        System.out.println("   *");
+        System.out.println("  *");
+        System.out.println(" *");
+        System.out.println("*****");
+    }
+}
