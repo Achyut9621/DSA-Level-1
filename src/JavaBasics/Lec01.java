9720f120a263b1aaf667af696f152cb9eabc82d9
Achyut Sudele <98205409+Sparkzud@users.noreply.github.com>
2025-08-04 23:45:34 +0530

    intial commit

---
+++ src/JavaBasics/Lec01.java
@@ -0,0 +1,9 @@
+package JavaBasics;
+
+public class Lec01 {
+    public static void main(String[] args)
+    {
+        System.out.print("Hello\n");
+        System.out.println("Hello,World");
+    }
+}
